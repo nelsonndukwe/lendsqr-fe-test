fb9b4e69699c070257bd57e6949f266fca15bbe4
nelson <nelsonndukwe800@gmail.com>
2026-01-31 07:21:04 +0100

    Add MobileToggle component and enhance layout styles
    - Introduced MobileToggle component for improved mobile sidebar functionality.
    - Updated layout.module.scss to include a floating button for the MobileToggle.
    - Enhanced layout styles for better responsiveness and usability.
    - Adjusted table styles to use max-height for tbody for improved layout consistency.

--- app/(dashboard)/dashboard/[orgId]/layout.tsx
+++ app/(dashboard)/dashboard/[orgId]/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/app/components/Navbar.tsx/nabar";
 import styles from "./scss/layout.module.scss";
 import Sidebar from "@/app/components/Sidebar/sidebar";
+import MobileToggle from "@/app/components/Toggle/mobile-toggle";
 export default function SetupLayout({
     children,
 }: {
@@ -12,6 +13,8 @@ export default function SetupLayout({
 
             <div className={styles.layoutWrapper}>
                 <Sidebar />
+
+                <MobileToggle />
 
                 <div className={styles.childrenWrapper}>{children}</div>
             </div>
---
+++ app/components/Toggle/mobile-toggle.tsx
@@ -0,0 +1,34 @@
+
+"use client"
+import { useDashboardState } from '@/hooks/use-dashboard';
+import { LuColumns2 } from 'react-icons/lu'
+import styles from "../../(dashboard)/dashboard/[orgId]/scss/layout.module.scss"
+
+const MobileToggle = () => {
+    const {
+        isMobile,
+        open,
+        openMobile,
+        toggleSidebar,
+        toggleMobileSidebar,
+        closeMobileSidebar,
+    } = useDashboardState();
+
+    console.log({openMobile})
+
+
+    return (
+        <button
+            onClick={() => toggleMobileSidebar()}
+            className={styles.floatingButton}
+        >
+            <LuColumns2 size={24} />
+        </button>
+    )
+}
+
+export default MobileToggle
+
+
+
+
